e9a3b6df8314bd70791f621d382792319d194a26
ngbntl <giabao5443@gmail.com>
2025-09-30 09:06:27 +0700

    header

--- src/components/layout/DefaultLayout/index.tsx
+++ src/components/layout/DefaultLayout/index.tsx
@@ -5,7 +5,7 @@ import { Header } from "../Header";
 export const DefaultLayout = () => {
   return (
     <div className="flex min-h-screen flex-col bg-[#f8f8f8]" role="main">
-      <Header />
+      {/* <Header /> */}
       <main className="flex-1 container mx-auto px-4 py-6 md:py-8 lg:py-10">
         <div className="max-w-7xl mx-auto">
           <Outlet />
